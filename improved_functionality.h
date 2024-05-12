24f51649ad32ddf7963b6f1a30e44b3cb7e6777b
Abdul Ahad Janjua <abdulahadjanjua1054@gmail.com>
2024-05-12 14:23:06 +0500

    final touching the project fixing cli

--- improved_functionality.h
+++ improved_functionality.h
@@ -5,7 +5,13 @@
 #include<ctime>
 #include<cstdlib>
 #include<vector>
+#include <ctime>
+#include <sstream>
+#include"SFML/System.hpp"
+#include"SFML/Graphics.hpp"
+#include"SFML/Window.hpp"
 using namespace std;
+using namespace sf;
 class MajorUsedFunctions
 {
 public:
@@ -24,7 +30,22 @@ public:
     string post_comments_file_path = "post_comments.txt";
     string post_likedusers_file_path = "_post_liked_count.txt";
     string page_owner_file_path = "_page_owner_PG_O.txt";
-    string timeline_file_path = "timeline.txt";
+    string timeline_post_file_path = "timeline.txt";
+    string post_timeline_file_path = "post_timeline.txt";
+    string getCurrentLocalTimeAsString(int i) {
+        // Get the current time
+        time_t now;
+        struct tm current_time;
+        time(&now);
+        localtime_s(&current_time, &now);
+
+        // Convert current time to string
+        stringstream ss;
+        ss << (current_time.tm_year + 1900) << '-'
+            << (current_time.tm_mon + 1) << '-'
+            << current_time.tm_mday-i;
+        return ss.str();
+    }
     bool DuplicateCheck(const string& _file_path, const string& _search)
     {
         ifstream file(_file_path);
@@ -113,14 +134,13 @@ public:
         string _temp;
         while (file >> _temp)
         {
-            if (storage.size() + 1 >= storage.capacity())
-            {
-                storage.resize(storage.size() + 1);
-            }
             if (_temp == search_word)
             {
                 file >> _temp;
-                storage.push_back(_temp);
+                if (!_temp.empty())
+                {
+                    storage.push_back(_temp);
+                }
             }
         }
         file.close();
@@ -138,13 +158,17 @@ public:
     }
     void writetofile(string filepath,string firxt,string second)
     {
-        ofstream file(filepath, ios::app);
-        Tokenizer(firxt);
-        Tokenizer(second);
-        file << firxt << ' ' << second << '\n';
-        file.close();
-        Detokenizer(firxt);
-        Detokenizer(second);
+        if (!firxt.empty()&&!second.empty())
+        {
+            ofstream file(filepath, ios::app);
+            Tokenizer(firxt);
+            Tokenizer(second);
+            file << firxt << ' ' << second << '\n';
+            file.close();
+            Detokenizer(firxt);
+            Detokenizer(second);
+        }
+       
     }
 };
 class User : public MajorUsedFunctions
@@ -163,11 +187,12 @@ public:
     {
         ID = IDassigner(datatype, user_name_file_path);
         cout << "Enter your name\n";
+        cin.ignore();
         getline(cin, name);
         cout << "Your unique ID is \t" << ID << endl;
         cout << "Remember your ID you can use it to access the app\n";
         cout << "Set date in format DD-MM-YYYY\n";
-        getline(cin, Date);
+        Date = getCurrentLocalTimeAsString(0);
         ofstream file(user_name_file_path, ios::app);
         Tokenizer(name);
         file << ID << ' ' << name << '\n';
@@ -179,7 +204,7 @@ public:
         name = NextGetter(_ID, user_name_file_path);
         Detokenizer(name);
         cout << "Set date in format DD-MM-YYYY\n";
-        getline(cin, Date);
+        Date = getCurrentLocalTimeAsString(0);
     }
     ~User()
     {
@@ -259,12 +284,33 @@ public:
     Post(b*&ptr)
     {
         cout << "Enter your post context\n";
+        cin.ignore();
         getline(cin, context);
         ID = IDassigner("POST-", user_post_file_path);
         owner = ptr->ID;
         writetofile(user_post_file_path, owner, ID);
         writetofile(post_description_file_path, ID, context);
         writetofile(post_owner_file_path, ID, owner);
+        writetofile(timeline_post_file_path,ptr->Date,ID);
+        writetofile(post_timeline_file_path, ID, ptr->Date);
+
+
+    }
+    template<class b>
+    Post(b*& ptr,string Pid)
+    {
+        context = NextGetter(Pid, post_description_file_path);
+        context += "---reposted from ";
+        string org_owner = NextGetter(Pid, post_owner_file_path);
+        context += org_owner;
+        ID = IDassigner("POST-", user_post_file_path);
+        owner = ptr->ID;
+        writetofile(user_post_file_path, owner, ID);
+        writetofile(post_description_file_path, ID, context);
+        writetofile(post_owner_file_path, ID, owner);
+        writetofile(timeline_post_file_path, ptr->Date, ID);
+        writetofile(post_timeline_file_path, ID, ptr->Date);
+
 
     }
     ~Post()
@@ -286,6 +332,7 @@ public:
     {
         string temp;
         cout << "Enter your user ID\n";
+        cin.ignore();
         getline(cin, temp);
         if (DuplicateCheck(user_name_file_path, temp))
         {
@@ -299,14 +346,13 @@ public:
             main=nullptr;
         }
     }
-    void Signup(User*main)
+    void Signup(User*&main)
     {
         main = new User;
     }
     template<class c>
     void viewposts(c*&ptr)
     {
-        getwhatever(user_post_file_path, ptr->post, ptr->ID);
         for  (auto i:ptr->post)
         {
             cout << i << '\t' << NextGetter(i, post_description_file_path) << endl;
@@ -325,19 +371,23 @@ public:
     void createApost(E*&ptr)
     {
         Post a(ptr);
+
     }
     void createAcomment(User*&ptr,string pid)
     {
-        if (OccurenceCounter(pid,post_comments_file_path)<10)
+        if (OccurenceCounter(pid,post_comments_file_path)<10 && !pid.empty())
         {
             string commentID = IDassigner("COMMENT-", post_comments_file_path);
             string context;
             cout << "Enter context of comment\n";
+            cin.ignore();
             getline(cin, context);
             writetofile(user_comment_file_path, ptr->ID, commentID);
             writetofile(post_comments_file_path, pid, commentID);
             writetofile(comment_description_file_path, commentID, context);
             writetofile(comment_post_file_path, commentID, pid);
+            writetofile(post_timeline_file_path, commentID, ptr->Date);
+            writetofile(timeline_post_file_path, ptr->Date,commentID);
         }
         else
         {
@@ -346,17 +396,35 @@ public:
     }
     void likeApost(User*& ptr, string pid)
     {
-        if (OccurenceCounter(pid,user_likedPosts_file_Path)<10)
+        if (OccurenceCounter(pid,user_likedPosts_file_Path)<10 && !pid.empty())
         {
             writetofile(user_likedPosts_file_Path, ptr->ID, pid);
             writetofile(post_likedusers_file_path, pid, ptr->ID);
+            writetofile(timeline_post_file_path, "L-"+pid, ptr->Date);
+            writetofile(post_timeline_file_path, ptr->Date, "L-" + pid);
         }
     }
-    void Home(User*& ptr)
+    void Home(User*& ptr, vector<string> &friendPosts, vector<string> &pagePosts)
     {
+        if (ptr == nullptr) {
+            cout << "Error: User not logged in.\n";
+            return;
+        }
+
         // Get the friend list and liked posts of the user
         getwhatever(user_friend_file_path, ptr->friend_list, ptr->ID);
         getwhatever(user_likedPosts_file_Path, ptr->liked, ptr->ID);
+        if (ptr->friend_list.empty())
+        {
+            cout << "NO FRIENDS ADDED IN LIST\n";
+        } 
+        if (ptr->liked.empty())
+        {
+            cout << "NO LIKED POSTS EXIST\n";
+        }
+        // Get the current date and time
+        string currentDate = getCurrentLocalTimeAsString(0);
+        string hrdate = getCurrentLocalTimeAsString(1);
 
         // Temporary storage for pages liked by the user
         vector<string> tempPages;
@@ -366,61 +434,285 @@ public:
         for (auto i : ptr->liked)
         {
             t = NextGetter(i, post_owner_file_path);
-            if (t.substr(0,5) == "PAGE-")
+            if (t.substr(0, 5) == "PAGE-")
             {
                 tempPages.push_back(t);
             }
         }
 
-        // Display posts from friends
+        // Display posts from friends shared in the last 24 hours
         for (auto friendID : ptr->friend_list)
         {
             if (!friendID.empty())
             {
                 // Get posts for each friend
-                vector<string> friendPosts;
                 getwhatever(user_post_file_path, friendPosts, friendID.erase(0, 2));
 
-                // Display each friend's posts
+                // Display each friend's posts shared in the last 24 hours
                 for (auto postID : friendPosts)
                 {
                     if (!postID.empty())
                     {
-                        string temp = NextGetter(postID, post_description_file_path);
-                        cout << "Friend Post: " << postID << ' ' << temp << endl;
+                        string postDate = NextGetter(postID, post_timeline_file_path);
+                        if (postDate == currentDate || postDate==hrdate)
+                        {
+                            string temp = NextGetter(postID, post_description_file_path);
+                            cout << "Friend Post: " << postID << ' ' << temp << endl;
+                        }
+                    }
+                    else if (friendPosts.empty())
+                    {
+                        cout << "NO FRIENDS POSTS IN LAST 24 hrs\n";
+                        break;
                     }
                 }
             }
         }
 
-        // Display posts from liked pages
+        // Display posts from liked pages shared in the last 24 hours
         for (auto pageID : tempPages)
         {
             if (!pageID.empty())
             {
                 // Get posts for each liked page
-                vector<string> pagePosts;
                 getwhatever(user_post_file_path, pagePosts, pageID);
 
-                // Display each liked page's posts
+                // Display each liked page's posts shared in the last 24 hours
                 for (auto postID : pagePosts)
                 {
                     if (!postID.empty())
                     {
-                        string temp = NextGetter(postID, post_description_file_path);
-                        cout << "Liked Page Post: " << postID << ' ' << temp << endl;
+                        string postDate = NextGetter(postID, post_timeline_file_path);
+                        if (postDate == currentDate || postDate == hrdate)
+                        {
+                            string temp = NextGetter(postID, post_description_file_path);
+                            cout << "Liked Page Post: " << postID << ' ' << temp << endl;
+                        }
+                    }
+                    else if (pagePosts.empty())
+                    {
+                        cout << "NO PAGE POSTS IN LAST 24 hrs\n";
+                        break;
                     }
                 }
             }
         }
     }
+
+    void Loginwindow()
+    {
+        sf::RenderWindow window(sf::VideoMode(800, 800), "INSTA-M", sf::Style::Close);
+        sf::Event action;
+
+        // Heading
+        sf::Font f1;
+        f1.loadFromFile("font1.otf");
+        sf::Text T1;
+        T1.setFont(f1);
+        T1.setString("InstaM");
+        T1.setCharacterSize(80);
+        T1.setFillColor(sf::Color::White);
+        T1.setOutlineColor(sf::Color::Black); // Set outline color
+        T1.setOutlineThickness(5); // Set outline thickness
+        sf::FloatRect textRect1 = T1.getLocalBounds();
+        T1.setOrigin(textRect1.left + textRect1.width / 2.0f, textRect1.top + textRect1.height / 2.0f);
+        T1.setPosition(800 / 2, 100); // Centered at the top of the window
+
+        // Login button
+        sf::RectangleShape loginouter;
+        sf::Text T2;
+        T2.setFont(f1);
+        T2.setString("LOGIN");
+        T2.setCharacterSize(50);
+        T2.setFillColor(sf::Color::Black);
+        T2.setOutlineColor(sf::Color::White); // Set outline color
+        T2.setOutlineThickness(2); // Set outline thickness
+        sf::FloatRect textRect2 = T2.getLocalBounds();
+        T2.setOrigin(textRect2.left + textRect2.width / 2.0f, textRect2.top + textRect2.height / 2.0f);
+        T2.setPosition(800 / 2, 400); // Position login button text centered within the button
+
+        loginouter.setSize(sf::Vector2f(200, 50));
+        loginouter.setPosition(800 / 2 - 100, 350); // Adjusted position to center vertically
+        loginouter.setOutlineThickness(20);
+        loginouter.setFillColor(sf::Color::Red);
+        loginouter.setOutlineColor(sf::Color::Blue);
+
+        // Signup button
+        sf::RectangleShape signupouter;
+        sf::Text T3;
+        T3.setFont(f1);
+        T3.setString("SIGNUP");
+        T3.setCharacterSize(50);
+        T3.setFillColor(sf::Color::Black);
+        T3.setOutlineColor(sf::Color::White); // Set outline color
+        T3.setOutlineThickness(2); // Set outline thickness
+        sf::FloatRect textRect3 = T3.getLocalBounds();
+        T3.setOrigin(textRect3.left + textRect3.width / 2.0f, textRect3.top + textRect3.height / 2.0f);
+        T3.setPosition(800 / 2, 550); // Position signup button text centered within the button
+
+        signupouter.setSize(sf::Vector2f(200, 50));
+        signupouter.setPosition(800 / 2 - 100, 500); // Adjusted position to center vertically
+        signupouter.setOutlineThickness(20);
+        signupouter.setFillColor(sf::Color::Green);
+        signupouter.setOutlineColor(sf::Color::Blue);
+
+        while (window.isOpen())
+        {
+            while (window.pollEvent(action))
+            {
+                if (action.type == sf::Event::Closed)
+                {
+                    window.close();
+                }
+                else if (action.type == sf::Event::MouseButtonPressed)
+                {
+                    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                    sf::Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+                    if (loginouter.getGlobalBounds().contains(mousePosF))
+                    {
+                        loginouter.setOutlineColor(sf::Color::Cyan);
+                        loginouter.setOutlineThickness(25);
+                        loginouter.setFillColor(sf::Color::Yellow);
+                    }
+                    else if (signupouter.getGlobalBounds().contains(mousePosF))
+                    {
+                        signupouter.setOutlineColor(sf::Color::Cyan);
+                        signupouter.setOutlineThickness(25);
+                        signupouter.setFillColor(sf::Color::Yellow);
+                    }
+                }
+            }
+
+            window.clear(sf::Color::White);
+            window.draw(T1); // Draw heading
+            window.draw(loginouter);
+            window.draw(T2);
+            window.draw(signupouter);
+            window.draw(T3);
+            window.display();
+        }
+    }
+    void viewvector(vector<string>temp)
+    {
+        for (auto i : temp)
+        {
+            if (!i.empty())
+            {
+                cout << i << endl;
+            }
+        }
+    }
+    void getsuggestions(string file_path, vector<string>& storage,string sub,int start,int end)
+    {
+        ifstream file(file_path, ios::in);
+        string _temp;
+        while (file >> _temp)
+        {
+            if (_temp.substr(start,end) ==sub )
+            {
+                if (!_temp.empty())
+                {
+                    if (storage.size() + 1 >= storage.capacity())
+                    {
+                        storage.resize(storage.size() + 1);
+                    }
+                    storage.push_back(_temp);
+                }
+            }
+        }
+        file.close();
+    }
     void driver_run()
     {
         User* main = nullptr;
-        login(main);
-        Page* a = new Page(main);
-        createApost(a);
-        viewposts(a);
-        delete main;
+        Page* ptr = nullptr;
+        cout << "\t\tWELCOME TO CLI BASED SOCIAL MEDIA APP\n\t\tenter l for login\n\t\tenter s for signup\n";
+        char choice = ' '; cin >> choice;
+        if (choice=='l'||choice=='L')
+        {
+            login(main);
+        }
+        else if (choice == 's' || choice == 'S')
+        {
+            Signup(main);
+        }
+        if (main)
+        {
+            system("CLS");
+            while (choice!='0')
+            {
+                
+                cout << "\t\tMENU\n";
+                cout << "1->Enter 1 for home\n";
+                cout << "2->Enter 2 for profile\n";
+                cout << "3->Enter 3 for friend list\n";
+                cout << "4->Enter 4 for page list\n";
+                cout << "5->Enter 5 for suggestions\n";
+                cout << "6->Press 0 to exit menu\n";
+                cin >> choice;
+                if (choice=='1')
+                {
+                    vector<string> temp1;
+                    vector<string> temp2;
+                    system("CLS");
+                    Home(main,temp1,temp2);
+                    int index = 0;
+                    cout << "Enter index to like / comment on post\n";
+                    cin >> index;
+                    if (index<temp1.size())
+                    {
+                        cout << "What would you like to do\n";
+                        cout << "1->like (a)\n";
+                        cout << "2->comment (b)\n";
+                        cout << "3->Repost (c)\n";
+                        cin >> choice;
+                        if (choice=='a')
+                        {
+                            likeApost(main, temp1[index-1]);
+                        }
+                        else if (choice == 'b')
+                        {
+                            createAcomment(main, temp1[index-1]);
+                        }
+                        else if (choice=='c')
+                        {
+                            Post a(main, temp1[index-1]);
+                        }
+                    }
+                    else if (index > temp1.size())
+                    {
+                        cout << "What would you like to do\n";
+                        cout << "1->like (a)\n";
+                        cout << "2->comment (b)\n";
+                        cout << "3->Repost (c)\n";
+                        cin >> choice;
+                        if (choice == 'a')
+                        {
+                            likeApost(main, temp2[index- temp1.size()-1]);
+                        }
+                        else if (choice == 'b')
+                        {
+                            createAcomment(main, temp2[index - temp1.size()-1]);
+                        }
+                        else if (choice == 'c')
+                        {
+                            Post a(main, temp2[index - temp1.size()-1]);
+                        }
+                    }
+                }
+                else if (choice=='2')
+                {
+                    getwhatever(user_post_file_path, main->post, main->ID);
+                    viewposts(main);
+                    cout << "Options\n";
+                    cout << "1->View comments (a)\n";
+                    cout << "2->View likes (b)\n";
+                    cout << "3->View publishing date (c)\n";
+
+                }
+            }
+        }
+
     }
+    
 };
